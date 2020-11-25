08247de7eba6d10e5ef106ec0983f76fc7185541
Maksim Diubo <diubomaksim@gmail.com>
2020-11-25 19:14:03 +0300

    fix: use React.memo to prevent ModalSelect update

--- src/Components/ModalForms/ModalSelect/ModalSelect.tsx
+++ src/Components/ModalForms/ModalSelect/ModalSelect.tsx
@@ -1,4 +1,4 @@
-import React, { useState, ChangeEvent } from 'react'
+import React, { useState, ChangeEvent, memo } from 'react'
 import classnames from 'classnames'
 
 import genres from '../../../data/genres.json'
@@ -10,71 +10,70 @@ interface IModalSelectProps {
   checkedItems?: string
 }
 
-export const ModalSelect: React.FC<IModalSelectProps> = ({ checkedItems }) => {
-  const [isCheckboxesOpen, setIsCheckboxesOpen] = useState(false)
-  const [checkedGenres, setCheckedGenres] = useState(
-    checkedItems?.split(',') || []
-  )
+export const ModalSelect: React.FC<IModalSelectProps> = memo(
+  ({ checkedItems }) => {
+    const [isCheckboxesOpen, setIsCheckboxesOpen] = useState(false)
+    const [checkedGenres, setCheckedGenres] = useState(
+      checkedItems?.split(',') || []
+    )
 
-  const handleChecked = (event: ChangeEvent<HTMLInputElement>) => {
-    event.target.checked
-      ? setCheckedGenres((genres) => [
-          ...genres,
-          event.target.name.toLowerCase(),
-        ])
-      : setCheckedGenres(
-          checkedGenres.filter(
-            (genre) => genre !== event.target.name.toLowerCase()
+    const handleChecked = (event: ChangeEvent<HTMLInputElement>) => {
+      event.target.checked
+        ? setCheckedGenres((genres) => [
+            ...genres,
+            event.target.name.toLowerCase(),
+          ])
+        : setCheckedGenres(
+            checkedGenres.filter(
+              (genre) => genre !== event.target.name.toLowerCase()
+            )
           )
-        )
+    }
+
+    return (
+      <div className="modal-select">
+        <h3 className="modal-select__label">genre</h3>
+        <div className="modal-select__field">
+          <span className="modal-select__placeholder">
+            {checkedGenres.length ? checkedGenres.join(', ') : 'Select genre'}
+          </span>
+          <button
+            className={classnames({
+              'modal-select__btn': true,
+              'modal-select__btn_rotate': isCheckboxesOpen,
+            })}
+            onClick={() =>
+              visibilityToggle(isCheckboxesOpen, setIsCheckboxesOpen)
+            }
+          ></button>
+        </div>
+        {isCheckboxesOpen && (
+          <div className="modal-select__checkboxes">
+            {genres.genres.map((item) => {
+              return (
+                <label
+                  key={item.id}
+                  className="modal-select__checkbox-container"
+                  htmlFor={item.id}
+                >
+                  <input
+                    className="modal-select__checkbox"
+                    type="checkbox"
+                    id={item.id}
+                    name={item.genre}
+                    onChange={(event) => handleChecked(event)}
+                    defaultChecked={checkedGenres.includes(
+                      item.genre.toLowerCase()
+                    )}
+                  />
+                  <span className="modal-select__checkmark"></span>
+                  {item.genre}
+                </label>
+              )
+            })}
+          </div>
+        )}
+      </div>
+    )
   }
-
-  return (
-    <div className="modal-select">
-      <h3 className="modal-select__label">genre</h3>
-      <div className="modal-select__field">
-        <span className="modal-select__placeholder">
-          {checkedGenres.length ? checkedGenres.join(', ') : 'Select genre'}
-        </span>
-        <button
-          className={classnames({
-            'modal-select__btn': true,
-            'modal-select__btn_rotate': isCheckboxesOpen,
-          })}
-          onClick={() =>
-            visibilityToggle(isCheckboxesOpen, setIsCheckboxesOpen)
-          }
-        ></button>
-      </div>
-      <div
-        className={classnames({
-          'modal-select__checkboxes': true,
-          'modal-select__checkboxes_visible': isCheckboxesOpen,
-        })}
-      >
-        {genres.genres.map((item) => {
-          return (
-            <label
-              key={item.id}
-              className="modal-select__checkbox-container"
-              htmlFor={item.id}
-            >
-              <input
-                className="modal-select__checkbox"
-                type="checkbox"
-                id={item.id}
-                name={item.genre}
-                onChange={(event) => handleChecked(event)}
-                defaultChecked={checkedGenres.includes(
-                  item.genre.toLocaleLowerCase()
-                )}
-              />
-              <span className="modal-select__checkmark"></span>
-              {item.genre}
-            </label>
-          )
-        })}
-      </div>
-    </div>
-  )
-}
+)
