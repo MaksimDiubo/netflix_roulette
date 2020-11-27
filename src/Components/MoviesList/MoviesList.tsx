e9c190654df98ece7e8381f83c2d8c39c9f415ef
Maksim Diubo <diubomaksim@gmail.com>
2020-11-27 21:54:53 +0300

    refact: render movies by server data

--- src/Components/MoviesList/MoviesList.tsx
+++ src/Components/MoviesList/MoviesList.tsx
@@ -1,80 +1,65 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { MovieItem } from '../MovieItem'
-
-const fakeData = [
-  {
-    id: 337167,
-    title: 'Fifty Shades Freed',
-    release_date: '2018-02-07',
-    poster_path:
-      'https://image.tmdb.org/t/p/w500/3kcEGnYBHDeqmdYf8ZRbKdfmlUy.jpg',
-    genres: ['Drama', 'Romance'],
-  },
-  {
-    id: 269149,
-    title: 'Zootopia',
-    release_date: '2016-02-11',
-    poster_path:
-      'https://image.tmdb.org/t/p/w500/sM33SANp9z6rXW8Itn7NnG1GOEs.jpg',
-    genres: ['Animation', 'Adventure', 'Family', 'Comedy'],
-  },
-  {
-    id: 181808,
-    title: 'Star Wars: The Last Jedi',
-    release_date: '2017-12-13',
-    poster_path:
-      'https://image.tmdb.org/t/p/w500/kOVEVeg59E0wsnXmF9nrh6OmWII.jpg',
-    genres: ['Fantasy', 'Adventure', 'Science Fiction'],
-  },
-  {
-    id: 284054,
-    title: 'Black Panther',
-    release_date: '2018-02-13',
-    poster_path:
-      'https://image.tmdb.org/t/p/w500/uxzzxijgPIY7slzFvMotPv8wjKA.jpg',
-    genres: ['Action', 'Adventure', 'Fantasy', 'Science Fiction'],
-  },
-  {
-    id: 354912,
-    title: 'Coco',
-    release_date: '2017-10-27',
-    poster_path:
-      'https://image.tmdb.org/t/p/w500/eKi8dIrr8voobbaGzDpe8w0PVbC.jpg',
-    genres: ['Adventure', 'Comedy', 'Family', 'Animation'],
-  },
-  {
-    id: 333339,
-    title: 'Ready Player One',
-    release_date: '2018-03-28',
-    poster_path:
-      'https://image.tmdb.org/t/p/w500/pU1ULUq8D3iRxl1fdX2lZIzdHuI.jpg',
-    genres: ['Adventure', 'Science Fiction', 'Action'],
-  },
-]
 
 interface IModalListProps {
   onEditModalOpen: () => void
   onDeleteModalOpen: () => void
 }
 
+interface IMovie {
+  title: string
+  tagline: string
+  vote_average: number
+  vote_count: number
+  release_date: string
+  poster_path: string
+  overview: string
+  budget: number
+  revenue: number
+  runtime: number
+  genres: Array<string>
+  id: number
+}
+
 export const MoviesList: React.FC<IModalListProps> = ({
   onEditModalOpen,
   onDeleteModalOpen,
 }) => {
+  const [movies, setMovies] = useState([] as Array<IMovie>)
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch(
+        'http://localhost:4000/movies?search=tarantino'
+      )
+      const parsedResponse = await response.json()
+      const { data } = parsedResponse
+      setMovies(data)
+    }
+    getData()
+  }, [])
+
+  if (!movies.length) {
+    return <p>Loading...</p>
+  }
+
   return (
     <>
-      {fakeData.map((item) => (
-        <MovieItem
-          key={item.id}
-          image={item.poster_path}
-          title={item.title}
-          date={item.release_date}
-          ganre={item.genres}
-          onEditModalOpen={onEditModalOpen}
-          onDeleteModalOpen={onDeleteModalOpen}
-        />
-      ))}
+      {movies.map((movie) => {
+        const { title, release_date, poster_path, genres, id } = movie
+        return (
+          <MovieItem
+            key={id}
+            image={poster_path}
+            title={title}
+            date={release_date}
+            ganre={genres}
+            onEditModalOpen={onEditModalOpen}
+            onDeleteModalOpen={onDeleteModalOpen}
+          />
+        )
+      })}
     </>
   )
 }
